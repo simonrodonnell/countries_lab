4523b1cb7d2b88a7443c71fa398ac7184baf7acd
Simon O'Donnell <simonrodonnell@yahoo.co.uk>
2019-10-22 15:30:27 +0100

    worked on extensions

--- src/app.js
+++ src/app.js
@@ -4,19 +4,39 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      allCountries: []
+      allCountries: [],
+      searchCountry: "",
+      selectedCountry: null,
+      favouriteCountries: [],
+      addCountry: ""
     },
     mounted(){
       this.fetchData();
     },
-    compute: {
-
+    computed: {
+      globalPopulation: function() {
+        return this.allCountries.reduce((runningTotal, country) => runningTotal + country.population, 0)
+      }
     },
     methods: {
       fetchData: function(){
         fetch("https://restcountries.eu/rest/v2/all")
         .then(response => response.json())
         .then(data => this.allCountries = data)
+      },
+      displayCountry: function(){
+        const selectedCountryArray = this.allCountries.filter((country) => {
+          return this.searchCountry.toLowerCase() === country.name.toLowerCase()
+        })
+        this.selectedCountry = selectedCountryArray[0]
+        return this.selectedCountry
+      },
+      addToFavouriteCountry: function(){
+        const selectedCountryArray = this.allCountries.filter((country) => {
+          return this.addCountry.toLowerCase() === country.name.toLowerCase()
+        })
+        const selectedCountry = selectedCountryArray[0]
+        this.favouriteCountries.push(selectedCountry)
       }
     }
   })
